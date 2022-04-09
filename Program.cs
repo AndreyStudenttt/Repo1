099c502ca788862ff5e2db4c393b1ae890c0daba
PIBO0120014 Ащеулов Андрей Витальевич <PIBO0120014@sibupk.su>
2022-04-09 22:37:51 +0700

    “Repo1start”

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine($"Введите год на проверку высокосности:");
+            int g = Convert.ToInt32(Console.ReadLine());
+            if ((g % 4) == 0)
+            {
+                Console.WriteLine($"{g}г. Является высокосным");
+            }
+            else
+            {
+                Console.WriteLine($"{g}г. Не является высокосным");
+            }
+        }
+    }
+}
